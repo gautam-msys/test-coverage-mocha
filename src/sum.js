9949916644e0451d1c69d500c8cf81f3f3370ece
gautam-msys <gkumar@msystechnologies.com>
2022-07-04 16:36:04 +0530

    removed unused functions

--- src/sum.js
+++ src/sum.js
@@ -1,14 +1,5 @@
 function sum(num1, num2) {
   return num1 + num2;
 }
-function getArgument(argument) {
-  return argument;
-}
-
-function getSecondArgument(arg1, arg2) {
-  return arg2;
-}
 
 module.exports = sum;
-module.exports = getArgument;
-module.exports = getSecondArgument;
