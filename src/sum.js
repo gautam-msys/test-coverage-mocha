92dbcf2ee0cd3a96ebb54cdaa49e0329cbd16281
gautam-msys <gkumar@msystechnologies.com>
2022-07-04 16:27:57 +0530

    added another function

--- src/sum.js
+++ src/sum.js
@@ -5,5 +5,10 @@ function getArgument(argument) {
   return argument;
 }
 
+function getSecondArgument(arg1, arg2) {
+  return arg2;
+}
+
 module.exports = sum;
 module.exports = getArgument;
+module.exports = getSecondArgument;
