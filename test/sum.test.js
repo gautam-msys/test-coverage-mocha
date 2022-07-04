6ec81dfb741156154dcd454b9121be04119fd389
gautam-msys <gkumar@msystechnologies.com>
2022-07-04 15:57:03 +0530

    initial code commit

---
+++ test/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('../sum');
+const expect = require('chai').expect;
+
+describe('Testing the Cube Functions', function () {
+  it('Sum of two numbers', function (done) {
+    expect(sum(1, 2)).be.a('number');
+    done();
+  });
+});
